ac195eca531018f40325939169652812830a29cf
Antoni Sobkowicz <rughalt@gmail.com>
2022-03-15 19:58:56 +0100

    Fix exp value for 1/8 and 1/3

--- module/actor/sheets/npc.js
+++ module/actor/sheets/npc.js
@@ -52,7 +52,11 @@ export class ActorSheetPFNPC extends ActorSheetPF {
     const total = parseFloat(data.data.details.totalCr || 0);
     data.labels.cr = CR.fromNumber(cr);
     data.labels.totalCr = CR.fromNumber(total)
-    data.labels.totalExp = total*75*4;
+    if (data.labels.totalCr == "1/3") {
+      data.labels.totalExp = 100;
+    } else {
+      data.labels.totalExp = Math.round(total*75*4);
+    }
     return data;
   }
 
