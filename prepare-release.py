ee7e1b2e49dc64b3bf97e66e7e33e729e3c9c171
Antoni Sobkowicz <rughalt@gmail.com>
2022-04-15 17:46:37 +0200

    Release preparation script

---
+++ prepare-release.py
@@ -0,0 +1,23 @@
+import json
+import argparse
+import os
+import string
+import random
+import base64
+import sys
+from git import Repo
+
+version = sys.argv[1];
+
+repo = Repo(self.rorepo.working_tree_dir)
+assert not repo.bare
+
+with open('system.json') as json_file:
+    data = json.load(json_file)
+    data['version'] = version
+    
+with open("system.json", "w") as fp:
+    json.dump(data, fp, indent=4) 
+
+repo.git.add("system.json")
+repo.git.commit('-m', 'Release %s' % version, author='rughalt@gmail.com')
