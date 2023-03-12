66fc1da1cc83d456837e823b011b6fc213ff3709
Antoni Sobkowicz <rughalt@gmail.com>
2023-03-12 11:33:49 +0100

    Update for compendium browser

--- module/sidebar/compendium.js
+++ module/sidebar/compendium.js
@@ -5,41 +5,31 @@ export class CompendiumDirectoryPF extends CompendiumDirectory {
   constructor(...args) {
     super(...args);
 
-    this.compendiums = {
-      spells: new CompendiumBrowser({ type: "spells", entityType: "Item" }),
-      items: new CompendiumBrowser({ type: "items", entityType: "Item" }),
-      bestiary: new CompendiumBrowser({ type: "bestiary", entityType: "Actor" }),
-      feats: new CompendiumBrowser({ type: "feats", entityType: "Item" }),
-      enhancements: new CompendiumBrowser({ type: "enhancements", entityType: "Item" }),
-      buffs: new CompendiumBrowser({ type: "buffs", entityType: "Item" }),
-    };
+    this.compendium = new CompendiumBrowser({ type: "spells", entityType: "Item" });
     CompendiumDirectoryPF.browser = this;
   }
 
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
-      template: "systems/D35E/templates/sidebar/compendium.html"
+      template: "systems/D35E/templates/sidebar/compendium.html",
     });
   }
 
   activateListeners(html) {
     super.activateListeners(html);
 
-    html.find(".compendium-footer .compendium.spells").click(e => this._onBrowseCompendium(e, "spells"));
-    html.find(".compendium-footer .compendium.items").click(e => this._onBrowseCompendium(e, "items"));
-    html.find(".compendium-footer .compendium.bestiary").click(e => this._onBrowseCompendium(e, "bestiary"));
-    html.find(".compendium-footer .compendium.feats").click(e => this._onBrowseCompendium(e, "feats"));
-    html.find(".compendium-footer .compendium.enhancements").click(e => this._onBrowseCompendium(e, "enhancements"));
-    html.find(".compendium-footer .compendium.buffs").click(e => this._onBrowseCompendium(e, "buffs"));
+    html.find(".compendium-footer .compendium").click((e) => this._onBrowseCompendium(e, "spells", "Item"));
   }
 
-  _onBrowseCompendium(event, type) {
+  _onBrowseCompendium(event, type, entityType, filters = {}) {
     event.preventDefault();
 
-    this.compendiums[type]._render(true);
+    CompendiumDirectoryPF.browser.compendium.preset(type, entityType, filters);
+    this.compendium._render(true);
   }
 
-  static browseCompendium(type) {
-    CompendiumDirectoryPF.browser.compendiums[type]._render(true);
+  static browseCompendium(type, entityType, filters = {}) {
+    CompendiumDirectoryPF.browser.compendium.preset(type, entityType, filters);
+    CompendiumDirectoryPF.browser.compendium._render(true);
   }
 }
