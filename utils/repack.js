e3187869ea84162d03b9f101bcebb40037d6e1fc
Antoni Sobkowicz <rughalt@gmail.com>
2023-06-03 10:52:40 +0200

    Update repack.js

--- utils/repack.js
+++ utils/repack.js
@@ -4,8 +4,6 @@ const fs = require('fs');
 
 let rawdata = fs.readFileSync('system.json');
 let system = JSON.parse(rawdata);
-console.log(system['packs'])
-
 for (let i = 0; i < system['packs'].length; i++) {
   let packPath = system['packs'][i]['path'];
   let packNameFromPath = packPath.replace('./packs/', '');
