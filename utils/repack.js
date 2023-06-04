945a13ef781a4ebd85f137ec32585b37fb7d71f1
Antoni Sobkowicz <rughalt@gmail.com>
2023-06-04 13:31:22 +0200

    Update repack script

--- utils/repack.js
+++ utils/repack.js
@@ -4,10 +4,20 @@ const fs = require('fs');
 
 let rawdata = fs.readFileSync('system.json');
 let system = JSON.parse(rawdata);
+let packdir = 'test'
+fs.rmSync(packdir, { recursive: true, force: true });
 for (let i = 0; i < system['packs'].length; i++) {
   let packPath = system['packs'][i]['path'];
   let packNameFromPath = packPath.replace('./packs/', '');
-  console.log("Repacking "+packNameFromPath+"...")
-  exec("fvtt package pack "+packNameFromPath+" --input-directory source/"+packNameFromPath+" --output-directory packs/")
-  console.log("Repacking "+packNameFromPath+"...")
+  if (fs.existsSync("source/"+packNameFromPath)) {
+    let foundFiles = fs.readdirSync("source/" + packNameFromPath).length;
+    console.log(`Repacking ${packNameFromPath}... (${foundFiles} files found)`)
+    exec(
+        `fvtt package pack ${packNameFromPath} --input-directory source/${packNameFromPath} --output-directory ${packdir}/`)
+    console.log("Repacking " + packNameFromPath + " done")
+  } else {
+    console.log("Pack " + packNameFromPath + " not found, skipping repack")
+  }
 }
+let numberOfPacked = fs.readdirSync(packdir).length;
+console.log("Recreated " + numberOfPacked + " packs")
